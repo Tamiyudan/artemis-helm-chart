70a4284891a406ff0cfbcf283625e22cfbdf1a1e
Alex Patrascu <sanyoc@gmail.com>
2023-04-17 17:29:57 -0500

    changed email variable name

--- notification_channel.tf
+++ notification_channel.tf
@@ -1,11 +1,11 @@
 resource "google_monitoring_notification_channel" "email" {
-  count        = "${length(var.email)}"
+  count        = "${length(var.email_list)}"
   project      = var.project_id
   enabled      = true
-  display_name = "Send email to ${element(var.email, count.index)}"
+  display_name = "Send email to ${element(var.email_list, count.index)}"
   type         = "email"
   labels = {
-    email_address = "${element(var.email, count.index)}"
+    email_address = "${element(var.email_list, count.index)}"
   }
   force_delete = false
 }
